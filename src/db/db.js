f7cf376667c9b04e77417ef245a3ca28f85ad6fb
Sergey Ershov <ser60v@gmail.com>
2018-10-12 18:14:13 +0300

    remove trash

--- src/db/db.js
+++ src/db/db.js
@@ -1,17 +1,15 @@
-import mysql from 'promise-mysql';
-import { env } from '../lib/env';
-
-console.log(env.DB_DATABASE)
+import mysql from 'promise-mysql'
+import { env } from '../lib/env'
 
 const config = {
-  host: env.DB_HOST+'',
+  host: env.DB_HOST + '',
   port: parseInt(env.DB_PORT),
-  user: env.DB_USER+'',
-  password: env.DB_PASSWORD+'',
-  database: env.DB_DATABASE+'',
-  connectionLimit: 100,
-};
+  user: env.DB_USER + '',
+  password: env.DB_PASSWORD + '',
+  database: env.DB_DATABASE + '',
+  connectionLimit: 100
+}
 
-const pool = mysql.createPool(config);
+const pool = mysql.createPool(config)
 
-export default pool;
+export default pool
